c04f6037d439ab5cb99557e064fbab97846be120
Alexander Labrador <globehackers@gmail.com>
2025-02-08 12:24:24 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import animated from "tailwindcss-animated";
 
 // https://astro.build/config
 export default defineConfig({
