0b50012a7fee944a1d06087616ab343e00c88a90
Alexander Labrador <globehackers@gmail.com>
2025-02-09 01:20:16 +0100

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,4 @@
-import animations from 'tailwindcss-animated'
+import animations from 'tailwindcss-animated;
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
