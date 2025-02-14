9a893012be2c537277bcd82852a21711ae199c2a
Alexander Labrador <globehackers@gmail.com>
2025-02-14 22:38:56 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  integrations: [react()],
+  integrations: [react(), image()],
   vite: {
     plugins: [tailwindcss()]
   }
