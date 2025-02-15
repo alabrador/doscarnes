84eaca07dd3fe0965ed0c412c1a5c35e7d6ada23
Alexander Labrador <globehackers@gmail.com>
2025-02-15 19:57:17 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import image from '@astrojs/image';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://doscarnes.com',
   adapter: vercel(),
-  integrations: [react(), image()],
+  integrations: [react(), image(), sitemap(), robotsTxt()],
   vite: {
     plugins: [tailwindcss()]
   }
